97f230b1b1fb5699121aa2b0b2cfc5a329c61c7a
Victor Chgbo <chigbovcm2020@gmail.com>
2024-06-12 01:50:26 +0100

    My Personal Portfolio Page v1.2.0. Modified every section minus the education and testimonial. Added a new section for projects.

--- myport.js
+++ myport.js
@@ -1,48 +1,51 @@
-const lightNDarkBtn = document.getElementById('lightndark');
-const lndIcon = document.querySelector('#lightndark .far');
-const profilePic = document.querySelector('.profile_pic');
-const body = document.body;
-const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-
-if (body.classList.contains('light')) {
-    profilePic.src = 'images/profile_pic_1.jpg';
-}
-
-if (prefersDarkScheme.matches) {
-    document.body.classList.remove("light");
-    document.body.classList.add("dark");
-
-    lndIcon.classList.remove("fa-sun");
-    lndIcon.classList.add("fa-moon");
-
-    profilePic.src = 'images/profile_pic_2.jpg';
-} else {
-    document.body.classList.remove("dark");
-    document.body.classList.add("light");
-
-    lndIcon.classList.remove("fa-moon");
-    lndIcon.classList.add("fa-sun");
-
-    profilePic.src = 'images/profile_pic_1.jpg';
-}
-
-lightNDarkBtn.addEventListener('click', () => {
-    lndIcon.classList.toggle('fa-sun');
-    lndIcon.classList.toggle('fa-moon');
-
-    body.classList.toggle('light');
-    body.classList.toggle('dark');
-
-    if (!body.classList.contains('light')) {
-        profilePic.src = 'images/profile_pic_2.jpg';
-    } else {
-        profilePic.src = 'images/profile_pic_1.jpg';
+const projects = [
+    {
+        name: 'Flavourfiesta Bites',
+        description: "This was inspired by a restaurant's page with the aim of building a landing page.",
+        stack: ['html5', 'css3-alt', 'js'],
+        image: 'flavorfiesta.png',
+        link: 'https://flavorfiesta.vercel.app/',
+        github: 'https://github.com/chivicks-hazard/landing-page'
+    },
+    {
+        name: 'My Music Player',
+        description: "This is a Web-based music player that utilizes the power of vanilla JavaScript",
+        stack: ['html5', 'css3-alt', 'js'],
+        image: 'musicplayer.png',
+        link: 'https://music-player-eta-topaz.vercel.app/',
+        github: 'https://github.com/chivicks-hazard/music-player'
     }
+];
 
-})
+const carouselItems = document.querySelectorAll('.carousel-item[data-bs-toggle="modal"]');
+const modalName = document.querySelector('.modal-title');
+const modalText= document.querySelector('.modal-body p');
+const modalPicture = document.querySelector('.modal-body img');
+const modalGithub = document.querySelector('.modal-footer .fa-github');
+const modalLink = document.querySelector('.modal-footer .fa-arrow-up-right-from-square');
+const modalStackContainer = document.querySelector('.modal-footer .lang');
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+carouselItems.forEach((carouselItem, index) => {
+    carouselItem.addEventListener('click', (e) => {
+        modalName.textContent = projects[index].name;
+        modalText.textContent = projects[index].description;
+        modalPicture.src = `images/${projects[index].image}`;
+        modalPicture.alt = projects[index].name;
+        modalGithub.href = projects[index].github;
+        modalLink.href = projects[index].link;
+
+        modalStackContainer.innerHTML = '';
+
+        projects[index].stack.forEach(stackItem => {
+            const iconElement = document.createElement('i');
+            iconElement.classList.add('fab', `fa-${stackItem}`, 'd-inline-block', 'mx-1', 'fa-1x');
+            modalStackContainer.appendChild(iconElement);
+        });
+    });
+});
 
 const date = document.getElementById('date');
 
@@ -52,23 +55,3 @@ function changeYear() {
 }
 
 changeYear();
-
-const small = window.matchMedia('only screen and (min-width : 320px) and (max-width: 480px)');
-const medium = window.matchMedia('only screen and (min-width : 481px) and (max-width : 768px)');
-const tablet = window.matchMedia('only screen and (min-width : 769px) and (max-width : 1023px)');
-const PC = window.matchMedia('only screen and (min-width : 1024px)');
-const TV = window.matchMedia('only screen and (min-width : 1080px)');
-
-window.addEventListener('resize', () => {
-    if (small.matches || medium.matches || tablet.matches || PC.matches || TV.matches) {
-        const htmlElement = document.documentElement;
-    
-        // Get the computed style of the HTML element
-        const computedStyle = window.getComputedStyle(htmlElement);
-    
-        // Get the font size in pixels
-        const fontSize = computedStyle.getPropertyValue('font-size');
-    
-        console.log(fontSize);
-    }
-})
