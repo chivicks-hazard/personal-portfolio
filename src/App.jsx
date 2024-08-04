4e342ecb91e19b6541d00170f2388b5d1ab47c82
Victor Chgbo <chigbovcm2020@gmail.com>
2024-08-04 01:56:45 +0100

    Created React Revamp for my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-primary'>App</div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './css/index.css';
+import "@fontsource-variable/josefin-sans";
+import "@fontsource-variable/karla";	
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
