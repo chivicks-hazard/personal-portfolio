15aad9e4c060bda9b3adfd5b6e147f39b873d682
Victor Chgbo <chigbovcm2020@gmail.com>
2024-08-07 04:35:16 +0100

    Almost done wit the carousel. Having issues rendering stack icons

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,16 +1,107 @@
-import { FaArrowUpRightFromSquare, FaBootstrap, FaCss3Alt, FaGithub, FaHtml5, FaJs } from "react-icons/fa6";
+import { FaArrowUpRightFromSquare, FaBootstrap, FaCss3Alt, FaGithub, FaHtml5, FaJava, FaJs, FaReact } from "react-icons/fa6";
+import { RiTailwindCssFill } from "react-icons/ri";
 import flavorfiesta from "../assets/images/flavorfiesta.png";
+import cliConverter from "../assets/images/cli-converter.gif";
+import opay from "../assets/images/my-opay-clone.gif";
 
 const Carousel = () => {
+    const stacks = {
+        html: <FaHtml5 />,
+        css: <FaCss3Alt />,
+        js: <FaJs />,
+        bootstrap: <FaBootstrap />,
+        tailwindcss: <RiTailwindCssFill />,
+        react: <FaReact />,
+        java: <FaJava />
+    };
+
+    const projects = [
+        {
+            name: "Flavorfiesta Bites",
+            description: "This was inspired by a restaurant's page with the aim of building a landing page.",
+            stack: ["html", "css", "js", "bootstrap"],
+            image: flavorfiesta,
+            link: "https://flavorfiesta.vercel.app/",
+            github: "https://github.com/chivicks-hazard/landing-page"
+        },
+        {
+            name: "CLI Converter App",
+            description: "This is an app I was able to build with Java",
+            image: cliConverter,
+            stack: ["java"],
+            github: "https://github.com/chivicks-hazard/java-unit-converter"
+        },
+        {
+            name: "Opay Clone",
+            description: "This is a project used to exercise my skills in React and Tailwind CSS",
+            image: opay,
+            stack: ["react", "tailwindcss"],
+            link: "https://my-opay-clone.vercel.app/",
+            github: "https://github.com/chivicks-hazard/opay-clone"
+        }
+    ];
+
     return (
         <div className="carousel slide mt-5" id="projectCarousel" data-bs-ride="carousel">
-        <div className="carousel-inner">
-            <div className="carousel-item active">
+            <div className="carousel-inner">
+                {projects.map((project, index) => (
+                    <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+                        <div className="d-flex justify-content-center">
+                            <div className="card w-50">
+                                <h5 className="card-header">{project.name}</h5>
+                                <div className="card-body">
+                                    <img src={project.image} alt={project.name} className='card-img-top w-100 mb-3' />
+                                    <p className="card-text">{project.description}</p>
+                                </div>
+                                <div className="card-footer p-2 d-flex justify-content-between">
+                                    <div className="d-flex flex-column">
+                                        {project.link && (
+                                            <a href={project.link} target="_blank" className="btn text-center mb-1">
+                                                View Live Project <FaArrowUpRightFromSquare />
+                                            </a>
+                                        )}
+                                        <a href={project.github} target="_blank" className="btn text-center mt-1">
+                                            View Source <FaGithub />
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <h4 className="text-white">Built With:</h4>
+                                        {/* {project.stack.map((stack, idx) => (
+                                            <span key={idx} className={`icon ${stack}`}>
+                                                {React.cloneElement(stacks[stack], { className: `icon ${stack}` })}
+                                            </span>
+                                        ))} */}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {/* Controls */}
+            <button className="carousel-control-prev" type="button" data-bs-target="#projectCarousel" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#projectCarousel" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    );
+};
+
+export default Carousel;
+
+
+/* 
+    <div className="carousel-item active">
                 <div className="d-flex justify-content-center">
                     <div className="card w-50">
                         <h5 className="card-header">Car</h5>
                         <div className="card-body">
-                            <img src={flavorfiesta} alt="Flavorfiesta Bites" className='card-img-top w-100 mb-3' />
+                            <img src={opay} alt="Flavorfiesta Bites" className='card-img-top w-100 mb-3' />
                             <p className="card-text">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                                 Laborum omnis quis libero! Quam veniam, voluptatibus repellat nisi quod praesentium rem,
@@ -18,11 +109,12 @@ const Carousel = () => {
                             </p>
                         </div>
                         <div className="card-footer p-2 d-flex justify-content-between">
-                            <div>
-                                <button className="btn text-center me-1">View Live Project <FaArrowUpRightFromSquare /></button>
-                                <button className="btn text-center ms-1">View Source <FaGithub /></button>
+                            <div className="d-flex flex-column">
+                                <a className="btn text-center mb-1">View Live Project <FaArrowUpRightFromSquare /></a>
+                                <a className="btn text-center mt-1">View Source <FaGithub /></a>
                             </div>
                             <div>
+                                <h4 className="text-white">Built With: </h4>
                                 <FaHtml5 className="icon html" />
                                 <FaCss3Alt className="icon css" />
                                 <FaJs className="icon javascript" />
@@ -32,19 +124,4 @@ const Carousel = () => {
                     </div>
                 </div>  
             </div>
-        </div>
-
-        {/* Controls */}
-        <button className="carousel-control-prev" type="button" data-bs-target="#projectCarousel" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#projectCarousel" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-        </button>
-    </div>
-    )
-}
-
-export default Carousel
+*/
