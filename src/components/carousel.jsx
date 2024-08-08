67cca230a663fb7d3690541aedb0fa391039b7b7
Victor Chgbo <chigbovcm2020@gmail.com>
2024-08-08 12:33:36 +0100

    Fixed Carousel. Projects section is complete.

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { FaArrowUpRightFromSquare, FaBootstrap, FaCss3Alt, FaGithub, FaHtml5, FaJava, FaJs, FaReact } from "react-icons/fa6";
 import { RiTailwindCssFill } from "react-icons/ri";
 import flavorfiesta from "../assets/images/flavorfiesta.png";
@@ -8,7 +9,7 @@ const Carousel = () => {
     const stacks = {
         html: <FaHtml5 />,
         css: <FaCss3Alt />,
-        js: <FaJs />,
+        javascript: <FaJs />,
         bootstrap: <FaBootstrap />,
         tailwindcss: <RiTailwindCssFill />,
         react: <FaReact />,
@@ -19,8 +20,9 @@ const Carousel = () => {
         {
             name: "Flavorfiesta Bites",
             description: "This was inspired by a restaurant's page with the aim of building a landing page.",
-            stack: ["html", "css", "js", "bootstrap"],
+            stack: ["html", "css", "javascript", "bootstrap"],
             image: flavorfiesta,
+            interval: 5000,
             link: "https://flavorfiesta.vercel.app/",
             github: "https://github.com/chivicks-hazard/landing-page"
         },
@@ -28,6 +30,7 @@ const Carousel = () => {
             name: "CLI Converter App",
             description: "This is an app I was able to build with Java",
             image: cliConverter,
+            interval: 35000,
             stack: ["java"],
             github: "https://github.com/chivicks-hazard/java-unit-converter"
         },
@@ -35,6 +38,7 @@ const Carousel = () => {
             name: "Opay Clone",
             description: "This is a project used to exercise my skills in React and Tailwind CSS",
             image: opay,
+            interval: 35000,
             stack: ["react", "tailwindcss"],
             link: "https://my-opay-clone.vercel.app/",
             github: "https://github.com/chivicks-hazard/opay-clone"
@@ -45,7 +49,7 @@ const Carousel = () => {
         <div className="carousel slide mt-5" id="projectCarousel" data-bs-ride="carousel">
             <div className="carousel-inner">
                 {projects.map((project, index) => (
-                    <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+                    <div className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval={project.interval ? (project.interval) : null} key={index}>
                         <div className="d-flex justify-content-center">
                             <div className="card w-50">
                                 <h5 className="card-header">{project.name}</h5>
@@ -53,24 +57,26 @@ const Carousel = () => {
                                     <img src={project.image} alt={project.name} className='card-img-top w-100 mb-3' />
                                     <p className="card-text">{project.description}</p>
                                 </div>
-                                <div className="card-footer p-2 d-flex justify-content-between">
-                                    <div className="d-flex flex-column">
+                                <div className="card-footer p-2 d-flex flex-column justify-content-center">
+                                    <div className="d-flex flex-row justify-content-center align-self-start mx-auto mb-2">
                                         {project.link && (
-                                            <a href={project.link} target="_blank" className="btn text-center mb-1">
+                                            <a href={project.link} target="_blank" className="btn text-center p-2 me-lg-1">
                                                 View Live Project <FaArrowUpRightFromSquare />
                                             </a>
                                         )}
-                                        <a href={project.github} target="_blank" className="btn text-center mt-1">
+                                        <a href={project.github} target="_blank" className="btn text-center p-2 ms-lg-1">
                                             View Source <FaGithub />
                                         </a>
                                     </div>
-                                    <div>
-                                        <h4 className="text-white">Built With:</h4>
-                                        {/* {project.stack.map((stack, idx) => (
-                                            <span key={idx} className={`icon ${stack}`}>
-                                                {React.cloneElement(stacks[stack], { className: `icon ${stack}` })}
-                                            </span>
-                                        ))} */}
+                                    <div className="d-flex flex-lg-column align-self-end mx-auto mt-2">
+                                        <h4 className="d-lg-block mx-lg-auto text-white align-self-start">Built With:</h4>
+                                        <div className="align-self-end">
+                                            {project.stack.map((stack, idx) => (
+                                                <span key={idx} className={`icon ${stack} mx-lg-2`}>
+                                                    {React.cloneElement(stacks[stack], { className: `icon ${stack}` })}
+                                                </span>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
